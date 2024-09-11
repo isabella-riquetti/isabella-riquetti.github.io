c47cc5edda3601cffa2eb44aa7a9c168c9ead58a
Isabella <isabella@sayprimer.com>
2024-09-10 21:35:02 -0300

    Add Screenshot of project

---
+++ assets/js/carrousel.js
@@ -0,0 +1,10 @@
+
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
--- assets/script.js
+++ assets/script.js
@@ -11,18 +11,22 @@ $(window).scroll(function () {
     } else {
         $('.instructions button').fadeIn('slow');
     }
-    
+
     toggleHeader();
 });
 
-$(window).ready(function() {
+$(window).ready(function () {
     updateCurrentTheme();
     addExperience();
     toggleHeader();
+
+    // new Glide('.glide', {
+    //     type: 'carousel'
+    // }).mount({ Images });
 });
 
 function toggleHeader() {
-    let fadePosition = window.innerWidth > 600 ? window.innerHeight-350 : 200;
+    let fadePosition = window.innerWidth > 600 ? window.innerHeight - 350 : 200;
     if ($(this).scrollTop() > fadePosition) {
         $('nav').fadeIn('slow');
     } else {
@@ -33,18 +37,18 @@ function toggleHeader() {
 function toggleMenu() {
     $(".menu").toggleClass("open");
     $(".menu").toggleClass("closed");
-    if($(".menu").hasClass("open")) {
-        let circleRadius = Math.sqrt((window.innerWidth*window.innerWidth)+((window.innerHeight)*(window.innerHeight)));
-        $(".nav-bg").css("height", circleRadius*2 + "px");
-        $(".nav-bg").css("width",circleRadius*2 + "px");
-        $(".nav-bg").css("top",circleRadius*-1 + "px");
-    } else {        
+    if ($(".menu").hasClass("open")) {
+        let circleRadius = Math.sqrt((window.innerWidth * window.innerWidth) + ((window.innerHeight) * (window.innerHeight)));
+        $(".nav-bg").css("height", circleRadius * 2 + "px");
+        $(".nav-bg").css("width", circleRadius * 2 + "px");
+        $(".nav-bg").css("top", circleRadius * -1 + "px");
+    } else {
         $(".nav-bg").css("height", "160px");
         $(".nav-bg").css("width", "160px");
         $(".nav-bg").css("top", "-80px");
     }
 }
 
-$(document).on("click",".options a", function(){
+$(document).on("click", ".options a", function () {
     toggleMenu();
 });
